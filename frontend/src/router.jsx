0ace1116316462143d8437b63a11e2716e590eaa
Mitchel Ogunniyi <isokandone@gmail.com>
2023-04-18 21:43:21 +0100

    guest layout and default layout created

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import Dashboard from "./views/Dashboard";
 import Login from "./views/Login";
 import Signup from "./views/Signup";
+import GuestLayout from "./components/GuestLayout";
+import { Children } from "react";
 
 
 const router = createBrowserRouter ([
@@ -14,13 +16,20 @@ const router = createBrowserRouter ([
         element : <Dashboard />
     },
     {
-        path : '/signup',
-        element : <Signup />
-    },
-    {
-        path : '/login',
-        element : <Login />
+        path : '/',
+        element : <GuestLayout /> ,
+        children : [
+            {
+                path : 'signup',
+                element : <Signup />
+            },
+            {
+                path : 'login',
+                element : <Login />
+            }
+        ]
     }
+
 ])
 
 export default router;
